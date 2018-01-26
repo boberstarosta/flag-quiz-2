c788881b0e7f43865f4ac300968505c99f1a3dd3
boberstarosta <boberstarosta@gmail.com>
2018-01-26 22:40:27 +0100

    Added over 200 countries

--- quiz/models.py
+++ quiz/models.py
@@ -7,17 +7,23 @@ from django.utils.safestring import mark_safe
 class Country(models.Model):
 	country_name = models.CharField(max_length=100)
 	population = models.IntegerField(default=0)
-	flag_image = models.ImageField(upload_to="flags")
+	flag_image = models.ImageField(upload_to="flags", blank=True, null=True)
 	
 	def __str__(self):
 		return self.country_name
 	
 	def flag_image_tag(self):
-		return mark_safe(u'<img src="%s" />' % self.flag_image.url)
+		if self.flag_image:
+			return mark_safe(u'<img src="%s" />' % self.flag_image.url)
+		else:
+			return ""
 	flag_image_tag.short_description = 'Flag image'
 	
 	def flag_thumbnail_tag(self):
-		return mark_safe(u'<img src="%s" height="30px" />' % self.flag_image.url)
+		if self.flag_image:
+			return mark_safe(u'<img src="%s" height="30px" />' % self.flag_image.url)
+		else:
+			return ""
 	flag_thumbnail_tag.short_description = 'Flag image'
 	
 	class Meta:
--- quiz/views.py
+++ quiz/views.py
@@ -4,6 +4,6 @@ from .models import Country
 
 
 def index(request):
-	all_countries = Country.objects.all()
+	all_countries = Country.objects.order_by("country_name").all()
 	context = {'all_countries': all_countries}
 	return render(request, 'index.html', context)
