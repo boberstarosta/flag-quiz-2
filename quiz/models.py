f9d2827eb20fc3d6ce42258314af0d56b8c95f8a
boberstarosta <boberstarosta@gmail.com>
2018-01-26 11:45:43 +0100

    Added models and migrations

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,18 @@
+import os
+
 from django.db import models
 
-# Create your models here.
+
+def get_image_path(instance, filename):
+	return os.path.join("images", str(instance.id), filename)
+
+
+class Flag(models.Model):
+	flag_image = models.ImageField(upload_to=get_image_path)
+
+
+class Country(models.Model):
+	flag = models.ForeignKey(Flag, on_delete=models.CASCADE)
+	country_name = models.CharField(max_length=100)
+	population = models.IntegerField(default=0)
+	
