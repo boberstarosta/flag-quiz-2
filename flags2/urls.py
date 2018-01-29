ca335c1690162a9d49acc4daea8a0a7b48e8b476
boberstarosta <boberstarosta@gmail.com>
2018-01-29 10:52:39 +0100

    Added social_django url

--- flags2/urls.py
+++ flags2/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 	path("", include("quiz.urls")),
     path('admin/', admin.site.urls),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ] +	static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
