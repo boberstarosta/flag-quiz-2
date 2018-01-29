ce48aae07ccaebd9b9006de86e9b187983d1c7aa
boberstarosta <boberstarosta@gmail.com>
2018-01-29 16:10:15 +0100

    Added accounts to urls

--- flags2/urls.py
+++ flags2/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 	path("", include("quiz.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] +	static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
