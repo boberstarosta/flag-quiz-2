ada687cb6ffda00f366f7218423ce1171714be90
boberstarosta <boberstarosta@gmail.com>
2018-01-29 10:43:58 +0100

    Added social_django.middleware.SocialAuthExceptionMiddleware to MIDDLEWARE

--- flags2/settings.py
+++ flags2/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'flags2.urls'
