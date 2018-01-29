d57327949b57011fbed3ef4d063016531f28271d
boberstarosta <boberstarosta@gmail.com>
2018-01-29 15:36:55 +0100

    Added auth url

--- flags2/urls.py
+++ flags2/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
 	path("", include("quiz.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] +	static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
